77e1a84dc7d91612c8d29b3aa4763a65c795df78
soundarig <gnanasoundari26@gmail.com>
2026-03-26 23:44:46 +0100

    vessel search api

--- app/models/ports.py
+++ app/models/ports.py
@@ -1,16 +1,14 @@
 from pydantic import BaseModel
 
-
 class PortResponse(BaseModel):
-    port_id: int
+    port_id: str
     port_code: str
     port_name: str
-    country_code: str | None = None
-    zone_code: str | None = None
-    is_eu_port: bool | None = None
+    port_type: str | None = None
+    geometry_type: str | None = None
+    Portterminal: bool | None = None
     latitude: float | None = None
     longitude: float | None = None
-    is_active: bool | None = None
 
 
 class PortsPageResponse(BaseModel):
